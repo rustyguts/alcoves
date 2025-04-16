fd9d60b8330c0338ad498ed94e716390370fdd04
Brendan Kennedy <9222118+rustyguts@users.noreply.github.com>
2025-04-15 22:00:56 -0400

    fix schema

--- app/src/db/db.ts
+++ app/src/db/db.ts
@@ -1,4 +1,9 @@
 import { ALCOVES_DB_PATH } from "$env/static/private";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
-export const db = drizzle(ALCOVES_DB_PATH);
+import * as asset from "./schema/asset";
+import * as user from "./schema/user";
+
+export const db = drizzle(ALCOVES_DB_PATH, {
+	schema: { ...asset, ...user },
+});
