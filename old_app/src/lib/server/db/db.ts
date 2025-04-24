94c56ec8b423d9d51b87b7d613894900cc9ed642
Brendan Kennedy <9222118+rustyguts@users.noreply.github.com>
2025-04-23 23:58:11 -0400

    Delete old app

--- old_app/src/lib/server/db/db.ts
+++
@@ -1,5 +0,0 @@
-import { ALCOVES_DB_PATH } from "$env/static/private";
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import * as schema from "./schema";
-
-export const db = drizzle(ALCOVES_DB_PATH, { schema });
--- old_app/src/types/ambient.d.ts
+++
@@ -1,5 +0,0 @@
-export interface AssetNotification {
-	type: "ASSET_CREATED" | "ASSET_UPDATED" | "ASSET_DELETED";
-	assets: Partial<Asset>[];
-	time: number;
-}
