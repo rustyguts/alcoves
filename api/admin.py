42619e55648c404b45a1d8382bf9b0d51e7c847b
Brendan Kennedy <brenwken@gmail.com>
2025-04-27 22:45:07 -0400

    Models

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Asset
 
+from .models import Asset, Library
 
 admin.site.register(Asset)
+admin.site.register(Library)
--- api/models.py
+++ api/models.py
@@ -1,17 +1,32 @@
 import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
-
-
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Asset(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     filename = models.CharField(max_length=255)
-    # Add other fields like upload_date, user, file_path etc. later
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
-# @receiver(post_save, sender=User)
-# def create_user_library(sender, instance, created, **kwargs):
-#     if created:
-#         Library.objects.create(owner=instance)
+class Library(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    assets = models.ManyToManyField(Asset, blank=True)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name="library")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def delete(self, *args, **kwargs):
+        if self.assets.exists():
+            raise Exception("Cannot delete library that contains assets.")
+        return super().delete(*args, **kwargs)
+
+
+@receiver(post_save, sender=User)
+def create_user_library(sender, instance, created, **kwargs):
+    if created:
+        Library.objects.create(owner=instance)
